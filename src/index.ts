e199883615fd7b83f806f8003a9fe552b9b26be9
im-neiru <neil.cantorne@protonmail.com>
2024-11-22 05:16:26 +0800

    feat(watcher): implement rebuild on change

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ export type PluginWasmPackOptions = {
   target: "nodejs" | "web" | "no-modules" | "deno" | "bundler";
 };
 
+let watcher: fs.FSWatcher | null = null;
+
 export const pluginWasmPack = (
   options: PluginWasmPackOptions
 ): RsbuildPlugin => ({
@@ -33,42 +35,78 @@ export const pluginWasmPack = (
       ".cargo/bin/wasm-pack"
     );
 
+    if (!fs.existsSync(wasmPackPath)) {
+      throw new Error(`wasm-pack not found please install it`);
+    }
+
+    if (!fs.existsSync(cratePath)) {
+      throw new Error(`${cratePath} does not exists`);
+    }
+
     api.onBeforeBuild(() => {
-      if (!fs.existsSync(wasmPackPath)) {
-        throw new Error(`wasm-pack not found please install it`);
-      }
-
-      if (!fs.existsSync(cratePath)) {
-        throw new Error(`${options.crate} does not exists`);
-      }
-
       console.log(`Building WASM crate at: ${cratePath}`);
 
-      const result = runSync(
-        wasmPackPath,
-        ["build", "--out-dir", outputPath, "--target", options.target],
-        {
-          stdio: "inherit",
-          cwd: cratePath,
-          env: {
-            ...process.env,
-            PATH: `${process.env.PATH}:${path.resolve(
-              process.env.HOME || "",
-              ".cargo/bin"
-            )}`,
-          },
-        }
-      );
-
-      if (result.error) {
-        throw new Error(`Failed to run wasm-pack: ${result.error.message}`);
-      }
-
-      if (result.status !== 0) {
-        throw new Error(`wasm-pack exited with status code ${result.status}`);
-      }
+      buildCrate(wasmPackPath, cratePath, outputPath, options.target);
 
       console.log("WASM build completed successfully!");
     });
+
+    api.onBeforeStartDevServer(() => {
+      const crateSrc = path.join(cratePath, "src");
+
+      if (!fs.existsSync(crateSrc)) {
+        throw new Error(`${crateSrc} does not exists`);
+      }
+
+      if (watcher) {
+        watcher.close();
+      }
+
+      watcher = fs.watch(crateSrc, { encoding: "buffer" }, (eventType) => {
+        if (eventType !== "change") return;
+
+        console.log(`Rebuilding WASM crate`);
+
+        buildCrate(wasmPackPath, cratePath, outputPath, options.target);
+
+        console.log("WASM build completed successfully!");
+      });
+    });
+
+    api.onCloseDevServer(() => {
+      watcher?.close();
+      watcher = null;
+    });
   },
 });
+
+function buildCrate(
+  wasmPackPath: string,
+  cratePath: string,
+  outputPath: string,
+  target: PluginWasmPackOptions["target"]
+) {
+  const result = runSync(
+    wasmPackPath,
+    ["build", "--out-dir", outputPath, "--target", target],
+    {
+      stdio: "inherit",
+      cwd: cratePath,
+      env: {
+        ...process.env,
+        PATH: `${process.env.PATH}:${path.resolve(
+          process.env.HOME || "",
+          ".cargo/bin"
+        )}`,
+      },
+    }
+  );
+
+  if (result.error) {
+    throw new Error(`Failed to run wasm-pack: ${result.error.message}`);
+  }
+
+  if (result.status !== 0) {
+    throw new Error(`wasm-pack exited with status code ${result.status}`);
+  }
+}
