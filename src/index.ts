9ae2d6bbdb4ee28fbb3e1c85debb945f4f6adea6
im-neiru <neil.cantorne@protonmail.com>
2024-11-22 01:46:09 +0800

    feat: implement initial build

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
 import type { RsbuildPlugin } from "@rsbuild/core";
+import { spawnSync } from "child_process";
+import path from "path";
 
 export type PluginWasmPackOptions = {
   crate: string;
 };
 
-export const pluginWasmPack = (): RsbuildPlugin => ({
+export const pluginWasmPack = (
+  options: PluginWasmPackOptions
+): RsbuildPlugin => ({
   name: "rsbuild:wasmpack",
-  setup: (api) => {},
+  setup: (api) => {
+    if (!(options?.crate?.length > 0)) {
+      throw new Error("Crate name is missing");
+    }
+
+    const cratePath = path.resolve(options.crate);
+
+    api.onBeforeStartDevServer(() => {
+      console.log(`Building WASM crate at: ${cratePath}`);
+
+      const result = spawnSync("/home/lien/.cargo/bin/wasm-pack", ["build"], {
+        cwd: cratePath,
+        stdio: "inherit",
+        shell: process.platform === "win32",
+      });
+
+      if (result.error) {
+        throw new Error(`Failed to run wasm-pack: ${result.error.message}`);
+      }
+
+      if (result.status !== 0) {
+        throw new Error(`wasm-pack exited with status code ${result.status}`);
+      }
+
+      console.log("WASM build completed successfully!");
+    });
+  },
 });
--- test/single-crate/index.test.ts
+++ test/single-crate/index.test.ts
@@ -2,37 +2,30 @@ import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { expect, test } from "@playwright/test";
 import { createRsbuild } from "@rsbuild/core";
-import { pluginWasmPack } from "../src/index";
-import { getRandomPort } from "../helper";
+import { pluginWasmPack } from "../../src/index";
+import { getRandomPort } from "../../helper";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-test("should render page as expected", async ({ page }) => {
+test("should build wasm", async ({ page }) => {
   const rsbuild = await createRsbuild({
     cwd: __dirname,
     rsbuildConfig: {
-      plugins: [pluginWasmPack()],
+      plugins: [
+        pluginWasmPack({
+          crate: "rust",
+        }),
+      ],
       server: {
         port: getRandomPort(),
       },
     },
   });
 
+  await rsbuild.build();
   const { server, urls } = await rsbuild.startDevServer();
 
-  await server.close();
-});
-
-test("should build succeed", async ({ page }) => {
-  const rsbuild = await createRsbuild({
-    cwd: __dirname,
-    rsbuildConfig: {
-      plugins: [pluginWasmPack()],
-    },
-  });
-
-  await rsbuild.build();
-  const { server, urls } = await rsbuild.preview();
+  await page.goto(urls[0]);
 
   await server.close();
 });
