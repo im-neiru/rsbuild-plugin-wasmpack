c56630df84640a6c5abe5e31640a71b96d899bc8
im-neiru <neil.cantorne@protonmail.com>
2024-11-23 04:10:57 +0800

    style: run formatter

--- test/single-crate/index.test.ts
+++ test/single-crate/index.test.ts
@@ -8,26 +8,26 @@ import { getRandomPort } from "../../helper";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 test("should build wasm", async ({ page }) => {
-  const rsbuild = await createRsbuild({
-    cwd: __dirname,
-    rsbuildConfig: {
-      plugins: [
-        pluginWasmPack({
-          crate: "test/single-crate/rust",
-          output: "pkg",
-          target: "nodejs",
-        }),
-      ],
-      server: {
-        port: getRandomPort(),
-      },
-    },
-  });
+	const rsbuild = await createRsbuild({
+		cwd: __dirname,
+		rsbuildConfig: {
+			plugins: [
+				pluginWasmPack({
+					crate: "test/single-crate/rust",
+					output: "pkg",
+					target: "nodejs",
+				}),
+			],
+			server: {
+				port: getRandomPort(),
+			},
+		},
+	});
 
-  await rsbuild.build();
-  const { server, urls } = await rsbuild.startDevServer();
+	await rsbuild.build();
+	const { server, urls } = await rsbuild.startDevServer();
 
-  await page.goto(urls[0]);
+	await page.goto(urls[0]);
 
-  await server.close();
+	await server.close();
 });
--- test/single-crate/src/index.js
+++ test/single-crate/src/index.js
@@ -1,1 +1,1 @@
-console.log("test wasm")
+console.log("test wasm");
